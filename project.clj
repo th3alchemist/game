ed5ed1fd867f167cf4a0a028ed10f659403dec7c
Ayo Arum <alchemist@Invidia.local>
2016-06-20 18:39:40 -0400

    added matrix dependancy

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :url "https://github.com/th3alchemist/game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.th3alchemist.clojure/matrix "0.1.0-SNAPSHOT"]])
